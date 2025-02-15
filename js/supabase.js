5d3f6a892c7f2c3d5614a8b44b27a35f602841ca
Tambe-yash <tambeyash124@gmail.com>
2025-02-15 18:29:11 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
-const supabaseUrl = 'https://sgcxoxqgkpkwdxhvwocp.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNnY3hveHFna3Brd2R4aHZ3b2NwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2MjA4MDcsImV4cCI6MjA1NTE5NjgwN30.q7P8HJY-erOW7AbFmDEqx9xMOdjEs6DMThO1UVZRc5g'
+const supabaseUrl = 'https://txbawtbwsebwlciujwkp.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR4YmF3dGJ3c2Vid2xjaXVqd2twIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2MjA1MTksImV4cCI6MjA1NTE5NjUxOX0.3WdkQRgUsz2sO0PHuzeN7MFYz8ieEgdedmu_uQOIDko'
 
 const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey)
 
